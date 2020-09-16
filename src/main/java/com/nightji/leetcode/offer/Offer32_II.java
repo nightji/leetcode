0d280ea65101afe924ed8d43b49749af821a2103
nightji <1056884474@qq.com>
2020-09-16 18:55:39 +0800

    Offer32_II

---
+++ src/main/java/com/nightji/leetcode/offer/Offer32_II.java
@@ -0,0 +1,41 @@
+package com.nightji.leetcode.offer;
+
+import com.nightji.leetcode.basic.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Offer32_II {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                temp.add(treeNode.val);
+                if (treeNode.left != null) {
+                    queue.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.add(treeNode.right);
+                }
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+}
