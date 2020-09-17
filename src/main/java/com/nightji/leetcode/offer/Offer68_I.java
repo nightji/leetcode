e666e9b80ae9f7f4f312e1846e1ab891645d3759
nightji <1056884474@qq.com>
2020-09-17 16:35:48 +0800

    Offer68_I

---
+++ src/main/java/com/nightji/leetcode/offer/Offer68_I.java
@@ -0,0 +1,22 @@
+package com.nightji.leetcode.offer;
+
+import com.nightji.leetcode.basic.TreeNode;
+
+public class Offer68_I {
+
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+
+}
