02a478611643d7a7b5c68cb64c9deae52a7bddc8
nightji <1056884474@qq.com>
2020-09-22 21:35:33 +0800

    Offer20

---
+++ src/main/java/com/nightji/leetcode/offer/Offer55_II.java
@@ -0,0 +1,25 @@
+package com.nightji.leetcode.offer;
+
+import com.nightji.leetcode.basic.TreeNode;
+
+public class Offer55_II {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
+}
