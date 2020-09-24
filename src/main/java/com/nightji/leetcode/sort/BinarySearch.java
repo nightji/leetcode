745292af8778e61a2d803e28ac9ab6f7f3ef36fd
nightji <1056884474@qq.com>
2020-09-24 22:40:42 +0800

    BinarySearch

--- src/main/java/com/nightji/leetcode/sort/BinarySearch.java
+++ src/main/java/com/nightji/leetcode/sort/BinarySearch.java
@@ -5,7 +5,7 @@ public class BinarySearch {
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int[] nums = {0, 1, 1, 2, 3, 3, 4, 5, 6, 6};
-        System.out.println(binarySearch.search(nums, 2));
+        System.out.println(binarySearch.searchRightTarget(nums, 1));
     }
 
     public int search(int[] array, int target) {
@@ -20,20 +20,44 @@ public class BinarySearch {
                 left = mid + 1;
             }
         }
-        return 0;
+        return -1;
     }
 
     public int searchLeftTarget(int[] array, int target) {
         int left = 0, right = array.length - 1;
         while (left <= right) {
-
+            int mid = left + (right - left) / 2;
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else if (array[mid] > target) {
+                right = mid - 1;
+            } else if (array[mid] == target) {
+                right = mid - 1;
+            }
         }
-
-        return 0;
+        if (left >= array.length || array[left] != target) {
+            return -1;
+        }
+        return left;
     }
 
     public int searchRightTarget(int[] array, int target) {
-        return 0;
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else if (array[mid] > target) {
+                right = mid - 1;
+            } else if (array[mid] == target) {
+                left = mid + 1;
+            }
+        }
+
+        if (right < 0 || array[right] != target) {
+            return -1;
+        }
+        return right;
     }
 
 
